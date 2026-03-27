c475ac9a097a81dfcb5e75a1600c993f8dc7b496
dasosann <goodtony0403@gmail.com>
2026-03-27 17:47:52 +0900

    feat: 코드리뷰 반영

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,10 @@
 // public/firebase-messaging-sw.js
 
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js",
+  "https://www.gstatic.com/firebasejs/12.8.0/firebase-app-compat.js",
 );
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js",
+  "https://www.gstatic.com/firebasejs/12.8.0/firebase-messaging-compat.js",
 );
 
 let messaging = null;
